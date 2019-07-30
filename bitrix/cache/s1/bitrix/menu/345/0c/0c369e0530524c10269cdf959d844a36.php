c009d16e8b1e196f45c5fdbff62d1386136f05e1
mbhusty <mbhusty@gmail.com>
2019-07-30 17:26:59 +0300

    rm cache

--- bitrix/cache/s1/bitrix/menu/345/0c/0c369e0530524c10269cdf959d844a36.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001564491792';
-$dateexpire = '001600491792';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:2:{s:8:"arResult";a:4:{i:0;a:10:{s:4:"TEXT";s:12:"Анонсы";s:4:"LINK";s:35:"/partneram/raspisanie-meropriyatiy/";s:8:"SELECTED";b:0;s:10:"PERMISSION";s:1:"X";s:16:"ADDITIONAL_LINKS";a:0:{}s:9:"ITEM_TYPE";s:1:"D";s:10:"ITEM_INDEX";i:0;s:6:"PARAMS";a:0:{}s:11:"DEPTH_LEVEL";i:1;s:9:"IS_PARENT";b:0;}i:1;a:10:{s:4:"TEXT";s:10:"Архив";s:4:"LINK";s:45:"/partneram/raspisanie-meropriyatiy/arkhiv.php";s:8:"SELECTED";b:1;s:10:"PERMISSION";s:1:"X";s:16:"ADDITIONAL_LINKS";a:0:{}s:9:"ITEM_TYPE";s:1:"P";s:10:"ITEM_INDEX";i:1;s:6:"PARAMS";a:0:{}s:11:"DEPTH_LEVEL";i:1;s:9:"IS_PARENT";b:0;}s:7:"menuDir";s:35:"/partneram/raspisanie-meropriyatiy/";s:8:"menuType";s:4:"left";}s:18:"templateCachedData";a:0:{}}}';
-return true;
-?>
